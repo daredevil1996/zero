ffee3128a36a8f9a49561c537e23f6cc4f280bff
Ashwin Ramesh <ashwinr64@gmail.com>
2018-03-17 11:27:48 +0530

    ASoc: msm: Import Xiaomi changes

--- sound/soc/msm/msm-dai-fe.c
+++ sound/soc/msm/msm-dai-fe.c
@@ -1,4 +1,5 @@
 /* Copyright (c) 2012-2017, The Linux Foundation. All rights reserved.
+ * Copyright (C) 2017 XiaoMi, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -2507,6 +2508,7 @@ static struct snd_soc_dai_driver msm_fe_dais[] = {
 	},
 	{
 		.capture = {
+<<<<<<< HEAD
 			.stream_name = "MultiMedia17 Capture",
 			.aif_name = "MM_UL17",
 			.rates = (SNDRV_PCM_RATE_8000_192000|
@@ -2666,6 +2668,39 @@ static struct snd_soc_dai_driver msm_fe_dais[] = {
 		.name = "MultiMedia29",
 		.probe = fe_dai_probe,
 	},
+=======
+			.stream_name = "QUIN MI2S_TX Hostless Capture",
+			.aif_name = "QUIN_MI2S_UL_HL",
+			.rates = SNDRV_PCM_RATE_8000_48000,
+			.formats = (SNDRV_PCM_FMTBIT_S16_LE |
+				SNDRV_PCM_FMTBIT_S24_LE),
+			.channels_min = 1,
+			.channels_max = 4,
+			.rate_min = 8000,
+			.rate_max = 48000,
+		},
+			.ops = &msm_fe_dai_ops,
+			.name = "QUIN_MI2S_TX_HOSTLESS",
+			.probe = fe_dai_probe,
+	},
+	{
+		.playback = {
+			.stream_name = "QUIN MI2S_RX Hostless Playback",
+			.aif_name = "QUIN_MI2S_DL_HL",
+			.rates = SNDRV_PCM_RATE_8000_192000,
+			.formats = SNDRV_PCM_FMTBIT_S16_LE |
+				SNDRV_PCM_FMTBIT_S24_LE,
+			.channels_min = 1,
+			.channels_max = 8,
+			.rate_min = 8000,
+			.rate_max = 192000,
+		},
+			.ops = &msm_fe_dai_ops,
+			.name = "QUIN_MI2S_RX_HOSTLESS",
+			.probe = fe_dai_probe,
+	},
+
+>>>>>>> 0368684... ASoc: msm: Import Xiaomi changes
 };
 
 static int msm_fe_dai_dev_probe(struct platform_device *pdev)
